1645f3fbc89dd125c00db965faaa79422c43e15b
Bryan W. <generalistcomputing@gmail.com>
2024-10-15 19:31:42 +0200

    updating webtoken

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 export function verifyToken(token: string) {
   try {
-    return jwt.verify(token, 'your_secret_key');
+    return jwt.verify(token, 'c2FjaGkvZGhoZWluaTlkZG93a3plZ29uZmRncmtyaW5ndGVy');
   } catch (error) {
     return null;
   }
